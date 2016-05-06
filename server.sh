7e5b8b34e38e8e7353cad834ac62cc432678ff55
Juan A. Fausd <juan.fausd@gmail.com>
2016-05-06 00:35:40 -0300

    Updated server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env node 
 
-./node_modules/nodus-server/bin/server.js helloworld $@ \
+./node_modules/nodus-server/bin/server.js index.js $@ \
 	| ./node_modules/bunyan/bin/bunyan
